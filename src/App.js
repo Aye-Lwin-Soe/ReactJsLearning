afe9c5604e8c082b0e03c6f0cf40710f246e33d8
Aye-Lwin-Soe <ayelwinsoe.it2018@gmail.com>
2026-06-14 18:48:09 +0630

    Extension and Documentation Add

--- src/App.js
+++ src/App.js
@@ -1,47 +1,10 @@
 import './App.css';
-import Footer from './components/Footer'
-import Header from './components/Header';
+import Extension from './components/Extension';
 
 function App() {
-    // One Way    
-    if (1 == 1) {
-        return (
-            <>
-                <Header />
-                <h1>This is Right Text</h1>
-            </>
-        );
-    } else {
-            return (
-                <>
-                    <Footer />
-                    <h1>This is Wrong Text</h1>
-                </>
-            )
-    }
-
-    //Two Way
     return (
         <>
-            { 1 == 10 ? <> <Header /><h1>This is Right Text</h1></> : <> <Footer /><h1>This is Wrong Text</h1></> }
-        </>
-    )
-    
-    //Three Way
-    return (
-        <>
-            { 
-                1 == 10 ? 
-                <div> 
-                    <Header />
-                    <h1>This is Right Text</h1>
-                </div> 
-                : 
-                <div> 
-                    <Footer />
-                    <h1>This is Wrong Text</h1>
-                </div> 
-            }
+            <Extension />
         </>
     )
 }
